bobg/bazel_gomock

commit fde78c91cf1783cc1e33ba278922ba67a6ee2a84

bring back but ignore interfaces on gomock_source (#54)
This fixes an old API break. It also adds some documentation to the
`source` argument.

gomock.bzl
@@ -72,6 +72,11 @@ _gomock_source = rule(
             doc = "The new Go file to emit the generated mocks into",
             mandatory = True,
         ),
+        "interfaces": attr.string_list(
+            allow_empty = False,
+            doc = "Ignored. If `source` is not set, this would be the list of Go interfaces to generate mocks for.",
+            mandatory = True,
+        ),
 	"aux_files": attr.string_list_dict(
             default = {},
             doc = "A map from packages to auxilliary Go source files to load for those packages.",

commit 95a203a9c33e18f7c19e039b73200277011743a0

Make "interfaces" optional, to match the documentation. This works around an apparent bug introduced in https://github.com/jmhodges/bazel_gomock/commit/fde78c91cf1783cc1e33ba278922ba67a6ee2a84.

gomock.bzl
@@ -84,9 +84,9 @@ _gomock_source = rule(
         "interfaces": attr.string_list(
             allow_empty = False,
             doc = "Ignored. If `source` is not set, this would be the list of Go interfaces to generate mocks for.",
-            mandatory = True,
+            mandatory = False,
         ),
-	"aux_files": attr.string_list_dict(
+	    "aux_files": attr.string_list_dict(
             default = {},
             doc = "A map from packages to auxilliary Go source files to load for those packages.",
         ),
@@ -223,7 +223,7 @@ _gomock_prog_gen = rule(
         "interfaces": attr.string_list(
             allow_empty = False,
             doc = "The names of the Go interfaces to generate mocks for. If not set, all of the interfaces in the library or source file will have mocks generated for them.",
-            mandatory = True,
+            mandatory = False,
         ),
         "mockgen_tool": attr.label(
             doc = "The mockgen tool to run",
@@ -283,7 +283,7 @@ _gomock_prog_exec = rule(
         "interfaces": attr.string_list(
             allow_empty = False,
             doc = "The names of the Go interfaces to generate mocks for. If not set, all of the interfaces in the library or source file will have mocks generated for them.",
-            mandatory = True,
+            mandatory = False,
         ),
         "package": attr.string(
             doc = "The name of the package the generated mocks should be in. If not specified, uses mockgen's default.",